0bcfc400492075001d4db8ce5687627a99400066
Hamdi Ghodbane <ghfalcon7@gmail.com>
2020-12-29 09:45:38 +0100

    Update load_test.sh

--- load_test.sh
+++ load_test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 INSTANCE=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
 rank=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name load_test --region eu-west-3  --query 'AutoScalingGroups[*].Instances[*].[InstanceId]' --output text |grep -n $INSTANCE | cut -d":" -f1)
 rank=$(printf "%02d" $rank)
